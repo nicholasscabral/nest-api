fc23d08ef1d821434484790226b786a0c852799d
nicholasscabral <nicholasscabral@gmail.com>
2021-11-17 20:35:04 -0300

    user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({ default: () => "now()" })
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
