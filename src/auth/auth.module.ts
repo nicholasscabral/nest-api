1ea67f593d065b06ad06d68e4b4953c6a3d439bc
nicholasscabral <nicholasscabral@gmail.com>
2021-11-18 18:57:10 -0300

    validating jwt (protecting routes)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { UsersModule } from "src/users/users.module";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt.strategy";
 import { LocalStrategy } from "./local.strategy";
 
 @Module({
@@ -16,7 +17,7 @@ import { LocalStrategy } from "./local.strategy";
       signOptions: { expiresIn: process.env.JWT_EXPIRESIN },
     }),
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
