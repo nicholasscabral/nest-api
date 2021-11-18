cc3bb228db67d0994397005f75403153e857988a
nicholasscabral <nicholasscabral@gmail.com>
2021-11-18 09:41:27 -0300

    config api documentation (swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const docConfig = new DocumentBuilder()
+    .setTitle("Nest-API")
+    .setVersion("1.0")
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, docConfig);
+
+  SwaggerModule.setup("/docs", app, doc);
+
   await app.listen(3000);
 }
 bootstrap();
