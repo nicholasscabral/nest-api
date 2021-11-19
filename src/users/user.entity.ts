ffcc166d9b8673068ef6f077fc6a77665cc22ef9
nicholasscabral <nicholasscabral@gmail.com>
2021-11-19 01:44:44 -0300

    user/vehicle relation (one to many)

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Vehicle } from "src/vehicles/vehicle.entity";
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
@@ -30,4 +32,7 @@ export class User {
 
   @UpdateDateColumn({ select: false })
   updated_at: Date;
+
+  @OneToMany((type) => Vehicle, (vehicle) => vehicle.user)
+  vehicles: Vehicle[];
 }
--- src/vehicles/vehicle.entity.ts
+++ src/vehicles/vehicle.entity.ts
@@ -1,34 +1,50 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from "src/users/user.entity";
 import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
+  JoinColumn,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
 
 @Entity("vehicles")
 export class Vehicle {
+  @ApiProperty()
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @ApiProperty()
   @Column({ unique: true })
   plate: string;
 
+  @ApiProperty()
   @Column()
   description: string;
 
+  @ApiProperty()
   @Column()
   color: string;
 
+  @ApiProperty()
   @Column()
   model: string;
 
+  @ApiProperty()
   @Column()
   location: string;
 
+  @ApiProperty()
   @CreateDateColumn({ default: () => "now()" })
   created_at: Date;
 
+  @ApiProperty()
   @UpdateDateColumn()
   updated_at: Date;
+
+  @ManyToOne((type) => User, (user) => user.vehicles, { onDelete: "CASCADE" })
+  @JoinColumn([{ name: "user_id", referencedColumnName: "id" }])
+  user: string;
 }
