2bb34cce99fd63f3fc28a287ebde2e40567c59a9
nicholasscabral <nicholasscabral@gmail.com>
2021-11-20 15:48:37 -0300

    config db on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import dbconfig from "../ormconfig";
-
 import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
 import { VehiclesModule } from "./vehicles/vehicles.module";
@@ -10,7 +8,20 @@ import { VehiclesModule } from "./vehicles/vehicles.module";
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(dbconfig),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      synchronize: false,
+      entities: ["dist/src/**/*.entity.js"],
+      migrations: ["dist/src/database/migrations/*.js"],
+      cli: {
+        migrationsDir: "src/database/migrations",
+      },
+    }),
     UsersModule,
     AuthModule,
     VehiclesModule,
